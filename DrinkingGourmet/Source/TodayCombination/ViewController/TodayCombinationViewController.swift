843dae5c50ca9f3453d6de30a53e6ef2633e46f0
moichi <zzikbu@gmail.com>
2024-01-17 08:33:24 +0900

    Chore: 오늘의 조합 폴더 생성

--- DrinkingGourmet/Source/TodayCombination/ViewController/TodayCombinationViewController.swift
+++ DrinkingGourmet/Source/TodayCombination/ViewController/TodayCombinationViewController.swift
@@ -1,16 +1,18 @@
 //
-//  TodayRecipeViewController.swift
+//  TodayCombinationViewController.swift
 //  DrinkingGourmet
 //
-//  Created by hwijinjeong on 1/6/24.
+//  Created by 이승민 on 1/17/24.
 //
 
 import UIKit
 
-class TodayRecipeViewController: UIViewController {
+class TodayCombinationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
+    
+
 }
