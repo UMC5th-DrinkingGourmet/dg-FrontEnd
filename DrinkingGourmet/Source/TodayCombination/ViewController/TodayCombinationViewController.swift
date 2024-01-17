b578b1af5253f465c30c0c89bc0749f4869a83b8
moichi <zzikbu@gmail.com>
2024-01-17 18:56:15 +0900

    홈 테이블뷰 UI 구현

--- DrinkingGourmet/Source/TodayCombination/ViewController/TodayCombinationViewController.swift
+++ DrinkingGourmet/Source/TodayCombination/ViewController/TodayCombinationViewController.swift
@@ -10,20 +10,27 @@ import SnapKit
 import Then
 
 class TodayCombinationViewController: UIViewController {
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         setupNaviBar()
+        setupTableView()
+        addViews()
+        configureConstraints()
     }
     
     // MARK: - 네비게이션바 설정
-    private func setupNaviBar() {
+    func setupNaviBar() {
         title = "오늘의 조합"
         
         navigationItem.searchController = UISearchController()
         navigationItem.searchController?.searchBar.placeholder = "~~를 입력하세요"
         navigationItem.searchController?.searchBar.setValue("취소", forKey: "cancelButtonText")
+        navigationItem.hidesSearchBarWhenScrolling = false
         
         // 돋보기 색상 변경
         let textField = navigationItem.searchController?.searchBar.value(forKey: "searchField") as! UITextField
@@ -42,5 +49,48 @@ class TodayCombinationViewController: UIViewController {
         let backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
         navigationItem.backBarButtonItem = backBarButtonItem
     }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.showsVerticalScrollIndicator = false // 스크롤바 숨기기
+        tableView.keyboardDismissMode = .onDrag // 스크롤 할 때 키보드 내리기
+        tableView.separatorStyle = .none // 테이블뷰 구분선 없애기
+        tableView.rowHeight = 247 //232
+        
+        tableView.register(TodayCombinationCell.self, forCellReuseIdentifier: "TodayCombinationCell")
+    }
+    
+    func addViews() {
+        self.view.addSubview(tableView)
+    }
+    
+    func configureConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(15)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.bottom.equalToSuperview()
+        }
+    }
+}
 
+extension TodayCombinationViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodayCombinationCell", for: indexPath) as! TodayCombinationCell
+        
+        cell.selectionStyle = .none // cell 선택 시 시각효과 제거
+        
+        return cell
+    }
+}
+
+extension TodayCombinationViewController: UITableViewDelegate {
+    
 }
