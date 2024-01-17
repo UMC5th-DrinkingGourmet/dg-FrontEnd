9238bcb8795102efeac33062c6c45461e897b6d3
moichi <zzikbu@gmail.com>
2024-01-18 00:34:58 +0900

    Chore: 디렉토리 구조 변경

---
+++ DrinkingGourmet/Source/TodayCombination/Detail/View/TodayCombinationDetailView.swift
@@ -0,0 +1,12 @@
+//
+//  TodayCombinationDetailView.swift
+//  DrinkingGourmet
+//
+//  Created by 이승민 on 1/18/24.
+//
+
+import UIKit
+
+class TodayCombinationDetailView: UIView {
+    
+}
---
+++ DrinkingGourmet/Source/TodayCombination/Detail/ViewController/TodayCombinationDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TodayCombinationDetailViewController.swift
+//  DrinkingGourmet
+//
+//  Created by 이승민 on 1/18/24.
+//
+
+import UIKit
+
+class TodayCombinationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+    }
+    
+    
+}
