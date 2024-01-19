85c781392f8d948ead640784b0360f7c82310743
moichi <zzikbu@gmail.com>
2024-01-20 06:47:59 +0900

    Chore: 상세화면 이미지 컬렉션뷰 파일 생성

---
+++ DrinkingGourmet/Source/TodayCombination/Home/View/TodayCombinationDetailCell.swift
@@ -0,0 +1,12 @@
+//
+//  TodayCombinationDetailCell.swift
+//  DrinkingGourmet
+//
+//  Created by 이승민 on 1/20/24.
+//
+
+import UIKit
+
+class TodayCombinationDetailCell: UICollectionViewCell {
+    
+}
