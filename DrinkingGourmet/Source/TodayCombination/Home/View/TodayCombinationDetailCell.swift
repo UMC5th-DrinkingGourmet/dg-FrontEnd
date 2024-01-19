e59952dc66aa352670fbb886fb00a8f204b57b9b
moichi <zzikbu@gmail.com>
2024-01-20 06:53:00 +0900

    Feat: 상세화면 이미지 컬렉션뷰 Cell 구현

--- DrinkingGourmet/Source/TodayCombination/Home/View/TodayCombinationDetailCell.swift
+++ DrinkingGourmet/Source/TodayCombination/Home/View/TodayCombinationDetailCell.swift
@@ -6,7 +6,34 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class TodayCombinationDetailCell: UICollectionViewCell {
     
+    let mainImage = UIImageView().then {
+        $0.image = UIImage(named: "img_community_today_detail")
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        contentView.addSubview(mainImage)
+    }
+    
+    private func configureConstraints() {
+        mainImage.snp.makeConstraints { make in
+            make.height.equalTo(375)
+            make.edges.equalTo(contentView)
+        }
+    }
 }
