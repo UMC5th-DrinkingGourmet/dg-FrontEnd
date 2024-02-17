cc4698d3b3371f8ffd4a0942b8fd24bbf3e4e2ff
moichi <zzikbu@gmail.com>
2024-02-18 03:46:43 +0900

    Feat: 오늘의 조합 업로드

---
+++ DrinkingGourmet/Source/TodayCombination/Upload/Model/CombinationUploadInput.swift
@@ -0,0 +1,15 @@
+//
+//  CombinationUploadInput.swift
+//  DrinkingGourmet
+//
+//  Created by 이승민 on 2/16/24.
+//
+
+struct CombinationUploadInput {
+    
+    struct fetchRecommendListDataInput: Codable {
+        var page: Int?
+        var size: Int?
+    }
+    
+}
