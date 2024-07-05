be8cd81e2240a5ff707007aa52c18e1e964cba57
moichi <zzikbu@gmail.com>
2024-07-05 21:13:19 +0900

    [#119] Refactor: 마이페이지 네트워킹 리팩토링

---
+++ DrinkingGourmet/Source/Service/MyPageService.swift
@@ -0,0 +1,129 @@
+//
+//  MyPageService.swift
+//  DrinkingGourmet
+//
+//  Created by 이승민 on 7/5/24.
+//
+
+import Alamofire
+
+final class MyPageService {
+    
+    static let shared = MyPageService()
+    private init() {}
+    
+    private let baseURL = "https://drink-gourmet.kro.kr"
+    
+    private func getHeaders() throws -> HTTPHeaders {
+        let accessToken = try Keychain.shared.getToken(kind: .accessToken)
+        return ["Authorization": "Bearer \(accessToken)"]
+    }
+    
+    // MARK: - 내 정보 조회
+    func getMyInfo (completion: @escaping (Swift.Result<MyInfoResponseDTO, Error>) -> Void) {
+        do {
+            let headers = try getHeaders()
+            
+            AF.request("\(baseURL)/users",
+                       method: .get,
+                       headers: headers)
+            .validate()
+            .responseDecodable(of: MyInfoResponseDTO.self) { response in
+                switch response.result {
+                case .success(let result):
+                    completion(.success(result))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            print("Failed to get access token: \(error.localizedDescription)")
+        }
+    }
+    
+    // MARK: - 나의 주류 추천 페이징 조회
+    func getMyRecommend (page: Int,
+                         size: Int,
+                         completion: @escaping (Swift.Result<MyRecommendResponseDTO, Error>) -> Void) {
+        do {
+            let headers = try getHeaders()
+            
+            let parameters : [String : Any] = [
+                "page" : page,
+                "size" : size
+            ]
+            
+            AF.request("\(baseURL)/recommends/list",
+                       method: .get,
+                       parameters: parameters,
+                       headers: headers)
+            .validate()
+            .responseDecodable(of: MyRecommendResponseDTO.self) { response in
+                switch response.result {
+                case .success(let result):
+                    completion(.success(result))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            print("Failed to get access token: \(error.localizedDescription)")
+        }
+    }
+    
+    // MARK: - 나의 오늘의 조합 페이징 조회
+    func getMyCombination (page: Int,
+                           completion: @escaping (Swift.Result<MyCombinationResponseDTO, Error>) -> Void) {
+        do {
+            let headers = try getHeaders()
+            
+            let parameters : [String : Any] = [
+                "page" : page
+            ]
+            
+            AF.request("\(baseURL)/combinations/my-page",
+                       method: .get,
+                       parameters: parameters,
+                       headers: headers)
+            .validate()
+            .responseDecodable(of: MyCombinationResponseDTO.self) { response in
+                switch response.result {
+                case .success(let result):
+                    completion(.success(result))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            print("Failed to get access token: \(error.localizedDescription)")
+        }
+    }
+    
+    // MARK: - 나의 레시피북 페이징 조회
+    func getMyRecipeBook (page: Int,
+                          completion: @escaping (Swift.Result<MyRecipeBookResponseDTO, Error>) -> Void) {
+        do {
+            let headers = try getHeaders()
+            
+            let parameters : [String : Any] = [
+                "page" : page
+            ]
+            
+            AF.request("\(baseURL)/recipes/my-page",
+                       method: .get,
+                       parameters: parameters,
+                       headers: headers)
+            .validate()
+            .responseDecodable(of: MyRecipeBookResponseDTO.self) { response in
+                switch response.result {
+                case .success(let result):
+                    completion(.success(result))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            print("Failed to get access token: \(error.localizedDescription)")
+        }
+    }
+}
