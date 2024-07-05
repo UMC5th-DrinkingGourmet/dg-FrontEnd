d215754e898a8bdef12c66b6cdf6917d71083e84
moichi <zzikbu@gmail.com>
2024-07-05 11:19:59 +0900

    [#128] Refactor: Tapman 뷰컨 구현

---
+++ DrinkingGourmet/Source/Like/ViewController/LikeCombinationViewController.swift
@@ -0,0 +1,90 @@
+//
+//  LikeCombinationViewController.swift
+//  DrinkingGourmet
+//
+//  Created by 이승민 on 7/5/24.
+//
+
+import UIKit
+
+class LikeCombinationViewController: UIViewController {
+    
+    let likeView = LikeView()
+    
+    // MARK: - View 설정
+    override func loadView() {
+        view = likeView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupRefresh()
+        setupCollectionView()
+    }
+    
+    private func setupRefresh() {
+        let rc = likeView.refreshControl
+        rc.addTarget(self, action: #selector(refreshTable(refresh:)), for: .valueChanged)
+        rc.tintColor = .customOrange
+        
+        likeView.collectionView.refreshControl = rc
+    }
+    
+    private func setupCollectionView() {
+        let cv = likeView.collectionView
+        cv.dataSource = self
+        cv.delegate = self
+        
+        cv.register(LikeCell.self, forCellWithReuseIdentifier: "LikeCell")
+    }
+    
+}
+
+// MARK: - Actions
+extension LikeCombinationViewController {
+    // 새로고침
+    @objc func refreshTable(refresh: UIRefreshControl) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+//            self.fetchData()
+            refresh.endRefreshing()
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension LikeCombinationViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LikeCell", for: indexPath) as! LikeCell
+        
+        
+        cell.thumbnailimage.backgroundColor = .red
+        cell.titleLabel.text = "테스트"
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension LikeCombinationViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow: CGFloat = 3
+        let paddingWidth = itemsPerRow - 1
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+}
