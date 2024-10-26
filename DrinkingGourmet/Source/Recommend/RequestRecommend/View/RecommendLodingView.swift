6742de63e059c0db85b2a46901f543dfa422a57f
moichi <zzikbu@gmail.com>
2024-10-26 23:37:32 +0900

    [#238] design: 주류추천 로딩 디자인, placeholder 수정

--- DrinkingGourmet/Source/Recommend/RequestRecommend/View/RecommendLodingView.swift
+++ DrinkingGourmet/Source/Recommend/RequestRecommend/View/RecommendLodingView.swift
@@ -10,20 +10,21 @@ import Lottie
 
 final class RecommendLodingView: UIView {
     // MARK: - View
+    let animationView: LottieAnimationView = .init(name: "Loader")
+    
+    let timeRequiredLabel = UILabel().then {
+        $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)
+        $0.text = "완벽한 조합을 찾고 있어요"
+        $0.textColor = UIColor(red: 0.56, green: 0.56, blue: 0.56, alpha: 1)
+    }
+    
     let nickNameLabel = UILabel().then {
         $0.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 24)
         $0.numberOfLines = 0
-        $0.lineBreakMode = .byWordWrapping
         var paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.25
-        $0.attributedText = NSMutableAttributedString(string: "\(UserDefaultManager.shared.userNickname)님을 위한 주류를\n선정하고 있어요", attributes: [NSAttributedString.Key.kern: -0.72, NSAttributedString.Key.paragraphStyle: paragraphStyle])
-    }
-    
-    let animationView: LottieAnimationView = .init(name: "Animation - 1707200937141")
-    
-    let timeRequiredLabel = UILabel().then {
-        $0.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 18)
-        $0.text = "완벽한 조합을 찾고있어요..."
+        paragraphStyle.alignment = .center
+        $0.attributedText = NSMutableAttributedString(string: "\(UserDefaultManager.shared.userNickname)님을 위한 주류를\n선정하고 있어요...", attributes: [NSAttributedString.Key.kern: -0.72, NSAttributedString.Key.paragraphStyle: paragraphStyle])
     }
     
     // MARK: - Init
@@ -42,28 +43,27 @@ final class RecommendLodingView: UIView {
     // MARK: - UI
     private func addViews() {
         self.addSubviews([
-            nickNameLabel,
             animationView,
+            nickNameLabel,
             timeRequiredLabel
         ])
     }
     
     private func configureConstraints() {
-        nickNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide).offset(45)
-            make.leading.equalToSuperview().inset(20)
-        }
-        
         animationView.snp.makeConstraints { make in
-            make.size.equalTo(90)
+            make.size.equalTo(100)
             make.centerX.equalToSuperview()
-            make.top.equalTo(nickNameLabel.snp.bottom).offset(200)
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(160)
         }
         
         timeRequiredLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(animationView.snp.bottom).offset(50)
+            make.centerX.equalTo(animationView)
+            make.top.equalTo(animationView.snp.bottom).offset(93)
+        }
+        
+        nickNameLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(animationView)
+            make.top.equalTo(timeRequiredLabel.snp.bottom).offset(8)
         }
     }
 }
-
